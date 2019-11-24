13f02691d0136fdb8e7bd437b7ab2ef1574a5123
Nicolas Emiliani <n.r.emiliani@gmail.com>
2019-11-24 16:04:29 -0300

    Renamed all __GC_ATLAS* defines to __DC_ATLAS*

--- src/device.h
+++ src/device.h
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#ifndef __GJ_ATLAS_DEVICE_H__
-#define __GJ_ATLAS_DEVICE_H__
+#ifndef __DC_ATLAS_DEVICE_H__
+#define __DC_ATLAS_DEVICE_H__
 #include <map>
 #include <string>
 #include <vector>
